e2690bdcbfb3be28df1ec92b8eb804a338b2b5f8
Rongbin Zhong <zhongrongbin2021@163.com>
2024-08-10 16:46:11 +0800

    继续学习信号，学习信号的各种函数

--- signal/mysignal.cc
+++ signal/mysignal.cc
@@ -6,29 +6,69 @@
 
 using namespace std;
 
+#define SIGNAL 2
+
 // void state()
 // {
 //     cout<<"请正确输入命令: 程序 数字 pid"<<endl;
 // }
 
-void CatchSign(int sign)
+//void CatchSign(int sign)
+//{
+//    cout<<"我捕捉了"<<sign<<"号信号"<<endl;
+//}
+
+void ShowPending(sigset_t& pending)
 {
-    cout<<"我捕捉了"<<sign<<"号信号"<<endl;
+    for(int i = 31; i>0; i--)
+    {
+        if(sigismember(&pending,i))
+        {
+            cout<<"1";
+        }
+        else
+        {
+            cout<<"0";
+        }
+    }
+    cout<<endl;
 }
 
 // ./mysignal -9 pid
 int main(int argc,char* argv[])
 {
-    for(int i = 1; i<=31 ;i++)
-    {
-        signal(i,CatchSign);
-    }
+    sigset_t block,oblock,pending;
+    
+    sigemptyset(&block);
+    sigemptyset(&oblock);
+    sigemptyset(&pending);
+
+    sigaddset(&block,SIGNAL);
+
+    sigprocmask(SIG_SETMASK,&block,&oblock);
 
     while(true)
     {
-        cout<<"hello linux: "<<getpid()<<endl;
+        sigemptyset(&pending);
+        sigpending(&pending);
+        ShowPending(pending);
         sleep(1);
     }
+
+    //int a = 100;
+    //int b = 0;
+    //int c = 100/0;
+
+    // for(int i = 1; i<=31 ;i++)
+    // {
+    //     signal(i,CatchSign);
+    // }
+
+    // while(true)
+    // {
+    //     cout<<"hello linux: "<<getpid()<<endl;
+    //     sleep(1);
+    // }
 
     // alarm(1);//设定一个闹钟，一秒后结束
 
