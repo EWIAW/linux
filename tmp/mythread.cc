4eff1be09fcf318ffdbf3831b1b3a7a5e6612049
Rongbin Zhong <zhongrongbin2021@163.com>
2024-09-15 22:06:22 +0800

    继续复习多线程并编写博客

--- tmp/mythread.cc
+++ tmp/mythread.cc
@@ -1,44 +1,46 @@
-#include <cstdio>
 #include <iostream>
 #include <vector>
-#include <pthread.h>
 #include <unistd.h>
-using namespace std;
-#define NUM 10
+#include "Thread.hpp"
 
-class ThreadData
-{
-public:
-    pthread_t tid;
-    char buffer[64];
-};
+using namespace std;
+
+int ticket = 10000; // 火车票的票数
+
+// pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 void *start_routine(void *args)
 {
-    int tmp = 10;
     while (true)
     {
-        cout << "我是" << (char *)args << "，我的tmp是" << tmp << "，tmp的地址是" << &tmp << endl;
-        sleep(1);
+        if (ticket > 0)
+        {
+            usleep(1000);//让打印慢一点，方便我们观察
+            cout << "我是" << (char *)args << "：我正在抢票：" << ticket-- << endl;
+        }
+        else
+        {
+            break;
+        }
     }
     return nullptr;
 }
 
 int main()
 {
-    vector<ThreadData *> vtid;
-    for (int i = 0; i < NUM; i++)
+    vector<Thread *> vt; // 定义容器存放线程Thread的地址
+    // 创建五个线程去抢票
+    for (int i = 0; i < 5; i++)
     {
-        ThreadData *td = new ThreadData;
-        snprintf(td->buffer, sizeof(td->buffer), "thread%d", i);
-        pthread_create(&td->tid, nullptr, start_routine, (void *)td->buffer);
-        vtid.push_back(td);
+        Thread *t = new Thread;
+        t->start(start_routine, (void *)t->getname().c_str());
+        vt.push_back(t);
     }
 
-    for (int i = 0; i < NUM; i++)
+    // 等待线程退出
+    for (int i = 0; i < 5; i++)
     {
-        pthread_join(vtid[i]->tid, nullptr);
+        vt[i]->join();
     }
-
     return 0;
 }
